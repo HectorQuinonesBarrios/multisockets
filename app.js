f4f91652fdec2947ddb5bc64aa64616f18eef84d
Hector <a291528@uach.mx>
2017-06-14 13:44:49 -0600

    Separated socket from the player and added movement

--- app.js
+++ app.js
@@ -9,28 +9,67 @@ app.get('/', (req, res, next)=>{
   res.sendFile(__dirname + '/public/index.html');
 });
 var SOCKET_LIST = {};
+var PLAYER_LIST = {};
+
+var Player =  (id)=>{
+  var self = {
+    x:250,
+    y:250,
+    id:id,
+    number: "" + Math.floor(10 * Math.random()),
+    pressingRight:false,
+    pressingLeft:false,
+    pressingUp:false,
+    pressingDown:false,
+    maxSpd:10
+  }
+  self.updatePosition = ()=>{
+    if(self.pressingRight)
+      self.x +=self.maxSpd;
+    if(self.pressingLeft)
+      self.x -=self.maxSpd;
+    if(self.pressingUp)
+      self.y -=self.maxSpd;
+    if(self.pressingDown)
+      self.y +=self.maxSpd;
+
+
+  }
+  return self;
+}
 
 io.sockets.on('connection', (socket)=>{
   console.log('Nuevo usuario');
 
   socket.id = Math.random();
-  socket.x = 0;
-  socket.y = 0;
   SOCKET_LIST[socket.id] = socket;
+  let player = Player(socket.id);
+  PLAYER_LIST[socket.id] = player;
   socket.on('disconnect',()=>{
     delete SOCKET_LIST[socket.id];
+    delete PLAYER_LIST[socket.id];
+  });
+  socket.on('keyPress',(data)=>{
+    if(data.inputId === 'left')
+      player.pressingLeft = data.state;
+    else if(data.inputId === 'right')
+      player.pressingRight = data.state;
+    else if(data.inputId === 'up')
+      player.pressingUp = data.state;
+    else if(data.inputId === 'down')
+      player.pressingDown = data.state;
   });
 });
 
 setInterval(()=>{
   var pack = [];
-  for(let i in SOCKET_LIST) {
-      var socket = SOCKET_LIST[i];
-      socket.x++;
-      socket.y++;
+  for(let i in PLAYER_LIST) {
+      let player = PLAYER_LIST[i];
+      player.updatePosition();
       pack.push({
-          x:socket.x,
-          y:socket.y
+          x:player.x,
+          y:player.y,
+          number:player.number
       });
     }
   for(let i in SOCKET_LIST){
